f2f0287c89d7913f9a3d29c6e5ae645fd55c67fa
Martin Heide <martin.heide@gmx.net>
2021-09-05 23:50:42 +0200

    Improve error examples

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require('express4');
+//const express = require('express5');
 const asyncHandler = require('express-async-handler');
 const fetch = require('node-fetch');
 
@@ -18,6 +19,26 @@ app.getAsync = function(route, handler) {
     app.get(route, asyncHandler(handler));
 }
 
+// ---------------------------------------------------------------------------
+
+app.get('/sqrt', (req, res) => {
+    const x = parseFloat(req.query.x);
+    if (!(x >= 0 && x <= Number.POSITIVE_INFINITY)) {
+        //res.statusCode = 400;
+        //return res.send({
+        //   'error': 'Non-negative number expected!'
+        //});
+        throw new Error('Non-negative number expected!');
+    }
+
+    res.send({
+        'x': x,
+        'sqrt(x)': Math.sqrt(x)
+    });
+});
+
+// ---------------------------------------------------------------------------
+
 function succeeds() {
     //return Promise.resolve('ok');
     return fetchJson('https://httpbin.org/json');
@@ -27,23 +48,41 @@ function fails() {
     return fetchJson('https://httpbin.org/status/404');
 }
 
-// OK
-app.get('/1', async (req, res, next) => {
-    res.send({ Message: await succeeds() });
+// ?err:
+// Express 4: No response
+// Express 5: Works
+app.get('/1a', (req, res, next) => {
+    const promise = req.query.err !== undefined ? fails() : succeeds();
+    promise.then((message) => {
+        res.send({ Message: message });
+    });
 });
 
-// Express 4: No response is sent :-(
-// Express 5: Works as expected   :-)
-app.get('/2', async (req, res, next) => {
+// ?err:
+// Express 4: No response
+// Express 5: Works
+app.get('/1b', async (req, res, next) => {
+    const promise = req.query.err !== undefined ? fails() : succeeds();
+    res.send({ Message: await promise });
+});
+
+// OK
+app.getAsync('/1aa', (req, res, next) => {
+    const promise = req.query.err !== undefined ? fails() : succeeds();
+    return promise.then((message) => {
+        res.send({ Message: message });
+    });
+});
+
+// OK
+app.getAsync('/1bb', async (req, res, next) => {
     res.send({ Message: await fails() });
 });
 
-// OK
-app.getAsync('/3', async (req, res, next) => {
-    res.send({ Message: await fails() });
-});
+// ---------------------------------------------------------------------------
 
 // Missing "await" - Promise has no impact on response.
+//
 // (node:636) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated.
 // In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
 app.getAsync('/4', async (req, res, next) => {
@@ -55,6 +94,8 @@ app.getAsync('/5', async (req, res, next) => {
     await fails();
     res.send({ Message: 'all done' });
 });
+
+// ---------------------------------------------------------------------------
 
 // Multiple promises - all required - sequential
 app.getAsync('/6', async (req, res, next) => {
@@ -75,6 +116,7 @@ app.getAsync('/7', async (req, res, next) => {
     res.send({ Message: 'all done' });
 });
 
+// ---------------------------------------------------------------------------
 
 // Error handler
 app.use((err, req, res, next) => {
